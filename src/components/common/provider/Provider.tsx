5c4864bd6fba1c70d26d52539fab13b9f96c1aa6
Sunwoo <dlatjsdn205@Naver.com>
2023-11-14 23:43:03 +0900

    Chore: 주석제거

--- src/components/common/provider/Provider.tsx
+++ src/components/common/provider/Provider.tsx
@@ -8,16 +8,6 @@ import { CookiesProvider } from "next-client-cookies";
 
 import store from "@/redux/config/configStore";
 
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       retry: false,
-//       refetchOnReconnect: false,
-//       refetchOnWindowFocus: false,
-//     },
-//   },
-// });
-
 export default function Provider({ children, cookieValue }: { children: React.ReactNode; cookieValue: any }) {
   const [queryClient] = useState(
     () =>
