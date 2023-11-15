2042facfe0ebdf854892d7d0030ae48e123cfee4
Sunwoo <dlatjsdn205@Naver.com>
2023-11-15 22:58:41 +0900

    fix: product 장바구니 담기 로직 변경

--- src/components/storeDetail/StoreIntro.tsx
+++ src/components/storeDetail/StoreIntro.tsx
@@ -10,7 +10,7 @@ export default function StoreIntro({ name, content, isOpen }: StoreIntro) {
   return (
     <section className="mb-10">
       <div className="bg-orange w-full h-[560px] mb-[23px]"></div>
-      <span className="text-sm font-semibold text-orange inline-block mb-[14px]">지금 영업중</span>
+      {isOpen && <span className="text-sm font-semibold text-orange inline-block mb-[14px]">지금 영업중</span>}
       <div className="flex items-center gap-2 mb-[14px]">
         <h2 className="text-xl font-bold">{name}</h2>
         <StatusIcon status="NEW" />
