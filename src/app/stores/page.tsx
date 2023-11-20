cbd44eefd37a19eefa1f8ab3402e28b85d0a0896
youjunho613 <njn613@gmail.com>
2023-11-20 22:36:45 +0900

    fix: 가게 목록 이미지 크기 고정

--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -119,7 +119,9 @@ export default function Stores() {
         {filterData().map((data) => (
           <li className="bg-white rounded-lg" key={data.storeId}>
             <Link className="w-full flex items-start gap-5 px-5 py-6 drag-none" href={`stores/${data.storeId}`}>
-              <Image src={data.storePictureUrl} alt="store" width={50} height={50} className="drag-none" />
+              <div className="h-[60px] w-[60px] relative">
+                <Image src={data.storePictureUrl} alt="store" fill className="drag-none" />
+              </div>
               <div>
                 <p className="text-base font-bold leading-[150%]">{data.name}</p>
                 <div className="flex gap-[6px] mt-2 text-[#787878] text-xs font-normal leading-[150%]">
