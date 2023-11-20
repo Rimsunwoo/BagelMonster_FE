35e10b7aee48ed7cd5c993d8b056ac0c65e4fbe8
youjunho613 <njn613@gmail.com>
2023-11-20 18:28:58 +0900

    fix: detail page

--- src/app/stores/[storeId]/page.tsx
+++ src/app/stores/[storeId]/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { useQuery } from "@tanstack/react-query";
 
 import { getStoreDetail } from "@/app/api/store";
 import StoreInfoTab from "@/components/storeDetail/StoreInfoTab";
 import StoreIntro from "@/components/storeDetail/StoreIntro";
+import { setStore } from "@/redux/modules/editStoreSlice";
 import isStoreOpen from "@/utils/isStoreOpen";
 interface StoreDetailProps {
   params: {
@@ -13,11 +17,17 @@ interface StoreDetailProps {
 }
 //#TODO 오픈시간 계산 함수, NEW 아이콘 범위지정 후 함수 생성
 export default function StoreDetail({ params: { storeId } }: StoreDetailProps) {
+  const dispatch = useDispatch();
+
   const {
     isPending,
     isError,
     data: storeDetailData,
   } = useQuery({ queryKey: ["storeDetail"], queryFn: () => getStoreDetail(storeId) });
+
+  useEffect(() => {
+    dispatch(setStore(storeDetailData));
+  }, [storeDetailData, dispatch]);
 
   if (isPending) return <h1>로딩중</h1>;
   if (isError || storeDetailData === undefined) return <h1>해당 가게를 찾을 수 없습니다</h1>;
